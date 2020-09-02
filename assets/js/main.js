e380d8b65bd25349ab9ed503971a80452c29f4ee
HerobrainGamer <herobraingaming@gmail.com>
2020-09-02 11:51:55 -0700

    code
    - created the bases of the webpage
    - will need to add more bases of members later
    - also need to locate the images for everyones bases
    - need to get something that is not a placeholder for the front page

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/*Setting the current year at the bottom of the page*/
+window.onload = function() {currnetYearFunction()};
+
+function currnetYearFunction() {
+    var y = new Date().getFullYear();
+    document.getElementById("currentYear").textContent = y;
+}
+
+/*Back to top java script functions*/
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
