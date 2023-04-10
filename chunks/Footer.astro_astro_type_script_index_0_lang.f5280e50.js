3fc4aef327cf9de7fed75836d7f23818bbf705bf
HerobrainGamer <herobraingaming@gmail.com>
2023-04-09 19:15:33 -0700

    v3.0
    - Rewrote the entire website using Astro.
    - Changed a few URL paths to be shorter.
    - Fixed an issue with certain member pages not loading and going to the 404 page.
    - Tweaks a lot of the CSS & JS files.
    - Added a /u/#name URL that will redirect to the right persons page as a measure to keep links to peoples pages shorter.
    - Also switched the site to be at https://www.pixelseries.xyz/

---
+++ chunks/Footer.astro_astro_type_script_index_0_lang.f5280e50.js
@@ -0,0 +1,1 @@
+var e=new Date().getFullYear().toString();document.getElementById("currentYear").textContent=e;
---
+++ hoisted.16f6d755.js
@@ -0,0 +1,1 @@
+import"./chunks/Footer.astro_astro_type_script_index_0_lang.f5280e50.js";
