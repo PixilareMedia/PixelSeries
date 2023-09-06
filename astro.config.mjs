c77f0fc1a02e00e52f9bba2446e5446f04a46b76
HerobrainGamer <herobraingaming@gmail.com>
2023-09-06 16:06:25 -0700

    Dev v3.5
    - Updated Astro.
    - Added Custom Items recipes.
    - Tweaked a few other things.
    - Removed some redundant code.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.pixelseries.xyz',
-    trailingSlash: 'always',
-    output: 'static',
-    format: 'file'
+  site: 'https://www.pixelseries.xyz',
+  trailingSlash: 'always',
+  output: 'static',
+  compressHTML: true,
+  build: {
+    format: 'directory'
+  },
+  server: {
+    port: 8083,
+    host: true
+  }
 });
