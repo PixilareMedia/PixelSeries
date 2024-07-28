9f9b04c5ce6dad352341a44110a5937e54897a64
HerobrainGamer <herobraingaming@gmail.com>
2024-07-28 00:13:10 -0700

    Dev 3.7
    - Added new Item Combinations recipes.
    - Added new Build Plate recipes & tweaked them to take into consideration the time of day & lunar events.
    - Added the brand new Mob Arena info.
    - Added the new members.
    - Changed the thunder storm to reprsent the new way of summoning the thunder storm.
    - Implemented a new Resource Pack /u/# link as the game server now sends the packs to the players.
    - Implemented some actual branding for this project instead of using the standard Pixilare Media logos.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.pixelseries.xyz',
+  site: 'https://pixelseries.xyz',
   trailingSlash: 'always',
   output: 'static',
   compressHTML: true,
